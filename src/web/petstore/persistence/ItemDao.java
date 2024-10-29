b0b2ccaeeb1d6d628e89cf6f6c6e778347e5f77a
“SuoLongGe” <17788818983@163.com>
2024-10-29 15:25:58 +0800

    Second

---
+++ src/web/petstore/persistence/ItemDao.java
@@ -0,0 +1,27 @@
+package web.petstore.persistence;
+
+import web.petstore.domain.Item;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ItemDao {
+
+    void updateInventoryQuantity(Map<String, Object> param){
+
+    }
+
+    public int getInventoryQuantity(String itemId){
+
+    }
+
+    public List<Item> getItemListByProduct(String productId){
+
+    }
+
+    public Item getItem(String itemId){
+
+    }
+}

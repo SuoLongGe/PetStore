639baf1b8c3e50d67f4bb0492057dff12165ade3
zyy <203423469@qq.com>
2024-11-05 10:51:12 +0800

    购物车

--- src/web/petstore/persistence/ItemDao.java
+++ src/web/petstore/persistence/ItemDao.java
@@ -16,7 +16,6 @@ import java.util.Map;
 public class ItemDao {
 
     private static final String GET_ITEMLIST_BY_PRODUCT = "SELECT I.ITEMID,LISTPRICE,UNITCOST,SUPPLIER AS supplierId,I.PRODUCTID AS productId,NAME AS productName,DESCN AS productDescription,CATEGORY AS categoryId,STATUS,ATTR1 AS attribute1,ATTR2 AS attribute2,ATTR3 AS attribute3,ATTR4 AS attribute4,ATTR5 AS attribute5 FROM ITEM I, PRODUCT P WHERE P.PRODUCTID = I.PRODUCTID AND I.PRODUCTID = ?";
-
     private static final String GET_ITEM = "select I.ITEMID,LISTPRICE,UNITCOST,SUPPLIER AS supplierId,I.PRODUCTID AS productId,NAME AS productName,DESCN AS productDescription,CATEGORY AS CategoryId,STATUS,ATTR1 AS attribute1,ATTR2 AS attribute2,ATTR3 AS attribute3,ATTR4 AS attribute4,ATTR5 AS attribute5,QTY AS quantity from ITEM I, INVENTORY V, PRODUCT P where P.PRODUCTID = I.PRODUCTID and I.ITEMID = V.ITEMID and I.ITEMID=?";
 
     private static final String GET_INVENTORY_QUANTITY = "SELECT QTY AS QUANTITY FROM INVENTORY WHERE ITEMID = ?";
